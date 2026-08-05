6eb320ecfc84d8c037178ee43b9a585d1ea8dabe
Joe Hunter <hunter.usmc@gmail.com>
2026-08-05 16:59:30 -0500

    Integrate snowauth_session for Snowflake login

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+from st_snowauth import snowauth_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -13,7 +14,7 @@ name_on_order = st.text_input("Name on Smoothie: ")
 st.write("The name on your Smoothie will be ", name_on_order)
 
 cnx = st.connection("snowflake")
-session = cnx.session()
+session = snowauth_session(label="Login with Snowflake") 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
